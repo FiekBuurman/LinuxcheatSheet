d3f9f9cc487ead7198a337269b653ad3751067b2
Fiek Buurman <80831799+FiekBuurman@users.noreply.github.com>
2023-03-27 22:04:58 +0200

    Update pihole_update.sh

--- Scripts/pihole_update.sh
+++ Scripts/pihole_update.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
-touch ../home/scripts/pihole_update.log
-sudo date > ../home/scripts/pihole_update.log
-echo "Start:" >> ../home/scripts/pihole_update.log
+touch ../root/scripts/pihole_update.log
+sudo date > ../root/scripts/pihole_update.log
+echo "Start:" >> ../root/scripts/pihole_update.log
 
 # update
-sudo apt-get update && sudo apt-get upgrade -y ../home/scripts/pihole_update.log 2>&1
+sudo apt-get update && sudo apt-get upgrade -y ../root/scripts/pihole_update.log 2>&1
 
 # remove
-sudo apt-get autoremove && apt-get autoclean ../home/scripts/pihole_update.log 2>&1
+sudo apt-get autoremove && apt-get autoclean ../root/scripts/pihole_update.log 2>&1
 
 # update pihole
-/usr/local/bin/pihole -up ../home/scripts/pihole_update.log 2>&1
-/usr/local/bin/pihole -g ../home/scripts/pihole_update.log 2>&1
+/usr/local/bin/pihole -up ../root/scripts/pihole_update.log 2>&1
+/usr/local/bin/pihole -g ../root/scripts/pihole_update.log 2>&1
 
 # write to text
-echo  "Last run" >> ../home/scripts/pihole_update.log
-sudo date >> ../home/scripts/pihole_update.log
-exit ../home/scripts/pihole_update.log 2>&1
+echo  "Last run" >> ../root/scripts/pihole_update.log
+sudo date >> ../root/scripts/pihole_update.log
+exit ../root/scripts/pihole_update.log 2>&1
