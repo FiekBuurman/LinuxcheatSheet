9f955d66506ed5f750f73a4b466d0f1cb7b7900e
Fiek Buurman <80831799+FiekBuurman@users.noreply.github.com>
2023-03-27 20:24:16 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,19 +1,23 @@
 echo "Starting setup.sh"
 source ~/.bashrc
-sleep 1
 /bin/rm -f setup.log
 /bin/rm -r -f scripts
 /bin/bash $HOME/.bashrc
-echo "running updates"
-apt update && apt ugrade -y >>setup.log 2>&1
-echo "installing apps"
-apt install -y bat >>setup.log 2>&1
-apt install -y sudo >>setup.log 2>&1
-echo "cleaning up"
-apt autoremove && apt autoclean >>setup.log 2>&1
+
 echo "creating dir ../root/scripts"
 mkdir ../root/scripts >>setup.log 2>&1
 cd ../root/scripts >>setup.log 2>&1
+
+echo "running updates"
+apt update && apt ugrade -y >>setup.log 2>&1
+
+echo "installing apps"
+apt install -y bat >>setup.log 2>&1
+apt install -y sudo >>setup.log 2>&1
+
+echo "cleaning up"
+apt autoremove && apt autoclean >>setup.log 2>&1
+
 echo "getting .bash_aliases"
 wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_aliases >>setup.log 2>&1
 echo ""
@@ -21,5 +25,4 @@ echo ""
 echo "  Adding bash_aliases to bashrc.."
 echo "[[ -f ../root/scripts/.bash_aliases ]] && . ../root/scripts/.bash_aliases" >> $HOME/.bashrc
 echo ""
-sleep 2
 source ~/.bashrc
