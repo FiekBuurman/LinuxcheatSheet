1e1a3835999fb9fadb3cd0bce37d05057fc403b7
Fiek Buurman <80831799+FiekBuurman@users.noreply.github.com>
2023-03-27 19:10:00 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -9,3 +9,11 @@ wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/
 wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_prompt
 /bin/bash /root/scripts/.bash_aliases
 /bin/bash /root/scripts/.bash_prompt
+
+
+
+echo ""
+echo ""
+echo "  Adding bash_aliases to bashrc"
+echo "[[ -f ../root/scripts/.bash_aliases ]] && . ../root/scripts/.bash_aliases" >> ../root/.bashrc
+echo ""
