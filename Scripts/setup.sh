05d42acd5040873c48bf59e8412eabc290d1256b
Fiek Buurman <80831799+FiekBuurman@users.noreply.github.com>
2023-03-27 19:29:07 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,12 +1,20 @@
-apt update && apt ugrade -y
-apt install -y bat
+echo "Starting setup.sh"
+echo "running updates"
+apt update && apt ugrade -y >>setup.log 2>&1
+echo "installing apps"
+apt install -y bat >>setup.log 2>&1
+apt install -y sudo >>setup.log 2>&1
+echo "cleaning up"
+apt autoremove && apt autoclean >>setup.log 2>&1
 
-
-mkdir ../root/scripts
-cd ../root/scripts
+echo "creating dir ../root/scripts"
+mkdir ../root/scripts >>setup.log 2>&1
+cd ../root/scripts >>setup.log 2>&1
+echo "getting .bash_aliases"
 #wget https://raw.githubusercontent.com/nallej/MyJourney/main/BashAddon.sh
-wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_aliases
-wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_prompt
+wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_aliases >>setup.log 2>&1
+echo "getting .bash_prompt"
+wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_prompt >>setup.log 2>&1
 #/bin/bash /root/scripts/.bash_aliases
 #/bin/bash /root/scripts/.bash_prompt
 
