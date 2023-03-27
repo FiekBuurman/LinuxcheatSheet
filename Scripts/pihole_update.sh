8f33920cf80b5a5fc7d1fbf988f19605f4201358
Fiek Buurman <80831799+FiekBuurman@users.noreply.github.com>
2023-03-27 22:18:59 +0200

    Update pihole_update.sh

--- Scripts/pihole_update.sh
+++ Scripts/pihole_update.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 touch ~/scripts/pihole_update.log
-sudo date > ~/scripts/pihole_update.log
-echo "Start:" >> ~/scripts/pihole_update.log
+date >~/scripts/pihole_update.log
+echo "Start:" >>~/scripts/pihole_update.log
 
 # update
-sudo apt-get update && sudo apt-get upgrade -y ~/scripts/pihole_update.log 2>&1
+apt-get update && apt-get upgrade -y >>~/scripts/pihole_update.log 2>&1
 
 # remove
-sudo apt-get autoremove && apt-get autoclean ~/scripts/pihole_update.log 2>&1
+sudo apt-get autoremove && apt-get autoclean >>~/scripts/pihole_update.log 2>&1
 
 # update pihole
-/usr/local/bin/pihole -up ~/scripts/pihole_update.log 2>&1
-/usr/local/bin/pihole -g ~/scripts/pihole_update.log 2>&1
+/usr/local/bin/pihole -up >>~/scripts/pihole_update.log 2>&1
+/usr/local/bin/pihole -g >>~/scripts/pihole_update.log 2>&1
 
 # write to text
 echo  "End Run" >> ~/scripts/pihole_update.log
