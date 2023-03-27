4671e27b334f70ab15cdc63dc844932d31cb03eb
Fiek Buurman <80831799+FiekBuurman@users.noreply.github.com>
2023-03-27 19:50:18 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -6,20 +6,14 @@ apt install -y bat >>setup.log 2>&1
 apt install -y sudo >>setup.log 2>&1
 echo "cleaning up"
 apt autoremove && apt autoclean >>setup.log 2>&1
-
 echo "creating dir ../root/scripts"
 mkdir ../root/scripts >>setup.log 2>&1
 cd ../root/scripts >>setup.log 2>&1
 echo "getting .bash_aliases"
-#wget https://raw.githubusercontent.com/nallej/MyJourney/main/BashAddon.sh
 wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_aliases >>setup.log 2>&1
-#echo "getting .bash_prompt"
-#wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_prompt >>setup.log 2>&1
-#/bin/bash /root/scripts/.bash_aliases
-#/bin/bash /root/scripts/.bash_prompt
-
 echo ""
 echo ""
-echo "  Adding bash_aliases to bashrc ofzo"
+echo "  Adding bash_aliases to bashrc"
 echo "[[ -f ../root/scripts/.bash_aliases ]] && . ../root/scripts/.bash_aliases" >> $HOME/.bashrc
 echo ""
+/bin/bash /root/scripts/.bash_aliases
