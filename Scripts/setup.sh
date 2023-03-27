e4c90319be2ceb78b1aa254a177c4f64359b4862
Fiek Buurman <80831799+FiekBuurman@users.noreply.github.com>
2023-03-27 19:12:27 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -7,10 +7,8 @@ cd ../root/scripts
 #wget https://raw.githubusercontent.com/nallej/MyJourney/main/BashAddon.sh
 wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_aliases
 wget https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/.bash_prompt
-/bin/bash /root/scripts/.bash_aliases
-/bin/bash /root/scripts/.bash_prompt
-
-
+#/bin/bash /root/scripts/.bash_aliases
+#/bin/bash /root/scripts/.bash_prompt
 
 echo ""
 echo ""
