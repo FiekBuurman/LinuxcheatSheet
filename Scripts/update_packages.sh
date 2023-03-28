dafed96d23e30341f280ab8fd8304178d1c69c41
fiekbuurman@gmail.com <fiekbuurman@gmail.com>
2023-03-28 09:19:30 +0200

    fixed vars

--- Scripts/update_packages.sh
+++ Scripts/update_packages.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 #bash <(curl https://raw.githubusercontent.com/FiekBuurman/LinuxcheatSheet/main/Scripts/update_packages.sh)
 log_path = ~/scripts/package_update.log
-echo "Start:" > log_path
-date >> log_path
+echo "Start:" > $log_path
+date >> $log_path
 
 # update
-apt-get update >> log_path 2>&1
-apt-get upgrade -y >> log_path 2>&1
+apt-get update >> $log_path 2>&1
+apt-get upgrade -y >> $log_path 2>&1
 
 # remove
-apt-get autoremove >> log_path 2>&1
-apt-get autoclean >> log_path 2>&1
+apt-get autoremove >> $log_path 2>&1
+apt-get autoclean >> $log_path 2>&1
 
-echo  "End Run..." >> log_path
-date >> log_path
+echo  "End Run..." >> $log_path
+date >> $log_path
 exit
