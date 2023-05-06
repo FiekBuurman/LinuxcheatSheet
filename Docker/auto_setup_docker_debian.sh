e31d9b165b1f7ca1a5086f3560a3d1da1d322a17
fiekbuurman@gmail.com <fiekbuurman@gmail.com>
2023-05-06 20:06:57 +0200

    added sudo to the script

--- Docker/auto_setup_docker_debian.sh
+++ Docker/auto_setup_docker_debian.sh
@@ -18,21 +18,23 @@ apt-get install \
   gnupg
 
 print_message "Creating keyrings"
-mkdir -m 0755 -p /etc/apt/keyrings
+sudo mkdir -m 0755 -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 print_message "Getting keyrings"
-echo \
+sudo echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 print_message "apt update"
-apt-get update
+sudo apt-get update
 
 print_message "Installing Docker"
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 print_message "Running Hello-World test"
-docker run hello-world
+sudo docker run hello-world
+sudo docker ps -a
+
